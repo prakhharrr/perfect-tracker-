c4969c0c0479f0a51bfa43179d7d7fe16353c3c6
prakhharrr <prakharverman6@gmail.com>
2026-01-08 12:26:19 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,43 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "tracker-app-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./part1.html",
+  "./part2.html",
+  "./part3.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
